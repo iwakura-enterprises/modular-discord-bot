0e2b2c57a7399f8937e7934fff494947f8618d8b
lilmayu <uwulilmayu@gmail.com>
2023-07-19 17:52:08 +0200

    Release b1.5.13

--- src/main/java/dev/mayuna/modularbot/ModularBot.java
+++ src/main/java/dev/mayuna/modularbot/ModularBot.java
@@ -242,7 +242,7 @@ public class ModularBot {
 
     public static class Values {
 
-        public static final String APP_VERSION = "b1.5.12";
+        public static final String APP_VERSION = "b1.5.13";
         private static @Getter @Setter String pathFolderModules = "./modules/";
         private static @Getter @Setter String pathFolderModuleConfigs = "./modules/%s/";
         private static @Getter @Setter String pathFolderJsonData = "./json_data/";
