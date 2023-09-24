a25380052af8872eeebc56fad703ebf3fac5a2ae
lilmayu <uwulilmayu@gmail.com>
2023-09-24 14:07:07 +0200

    made app_version not final due to modules having hardcoded modular bot version

--- src/main/java/dev/mayuna/modularbot/ModularBot.java
+++ src/main/java/dev/mayuna/modularbot/ModularBot.java
@@ -241,7 +241,7 @@ public class ModularBot {
 
     public static class Values {
 
-        public static final String APP_VERSION = "b1.5.14";
+        public static String APP_VERSION = "b1.5.14";
         private static @Getter @Setter String pathFolderModules = "./modules/";
         private static @Getter @Setter String pathFolderModuleConfigs = "./modules/%s/";
         private static @Getter @Setter String pathFolderJsonData = "./json_data/";
