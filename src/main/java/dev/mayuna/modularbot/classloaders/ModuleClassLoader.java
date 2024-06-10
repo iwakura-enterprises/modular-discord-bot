4a84e5db9e360c0dadbf7352029bc20d55cf1228
hokhanh <uwulilmayu@gmail.com>
2024-06-10 16:01:32 +0200

    fix: Removed the ability to read module's resources from foreign module's class loader

--- src/main/java/dev/mayuna/modularbot/classloaders/ModuleClassLoader.java
+++ src/main/java/dev/mayuna/modularbot/classloaders/ModuleClassLoader.java
@@ -1,13 +1,9 @@
 package dev.mayuna.modularbot.classloaders;
 
-import org.jetbrains.annotations.Nullable;
-
-import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.nio.file.Path;
-import java.util.Enumeration;
 import java.util.List;
 
 /**
@@ -32,61 +28,6 @@ public final class ModuleClassLoader extends URLClassLoader {
     public ModuleClassLoader(Path jarFile, ClassLoader parent, List<ModuleClassLoader> otherClassLoaders) throws MalformedURLException {
         super(new URL[] {jarFile.toUri().toURL()}, parent);
         this.otherClassLoaders = otherClassLoaders;
-    }
-
-    @Nullable
-    @Override
-    public URL getResource(String name) {
-        URL resource = findResource(name);
-
-        if (resource == null) {
-            synchronized (otherClassLoaders) {
-                for (ModuleClassLoader otherClassLoader : otherClassLoaders) {
-                    if (otherClassLoader == this) {
-                        continue;
-                    }
-
-                    resource = otherClassLoader.getResource(name);
-
-                    if (resource != null) {
-                        break;
-                    }
-                }
-            }
-        }
-
-        if (resource == null) {
-            resource = getParent().getResource(name);
-        }
-
-        return resource;
-    }
-
-    @Override
-    public Enumeration<URL> getResources(String name) throws IOException {
-        Enumeration<URL> resources = findResources(name);
-
-        if (!resources.hasMoreElements()) {
-            synchronized (otherClassLoaders) {
-                for (ModuleClassLoader otherClassLoader : otherClassLoaders) {
-                    if (otherClassLoader == this) {
-                        continue;
-                    }
-
-                    resources = otherClassLoader.getResources(name);
-
-                    if (resources.hasMoreElements()) {
-                        break;
-                    }
-                }
-            }
-        }
-
-        if (!resources.hasMoreElements()) {
-            resources = getParent().getResources(name);
-        }
-
-        return resources;
     }
 
     @Override
